16ee2d7f2fc40433cb2d84a4bfffc743ea883b04
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2023-10-13 23:46:13 +0800

    feat: reimplement new terminal

--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -8,6 +8,7 @@
 <script setup>
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit'
+import { AttachAddon } from 'xterm-addon-attach'
 import { ref, onMounted, reactive } from 'vue';
 import axios from "axios";
 
@@ -41,11 +42,6 @@ onMounted(async () => {
 
 const initTerminalWebSocket = async () => {
     const ws = new WebSocket(`ws://${location.hostname}:8080/api/v1/terminal/ws`)
-    ws.onmessage = (ev) => {
-        term.write("\n")
-        term.write(ev.data)
-        term.prompt()
-    }
     return new Promise((resolve, reject) => {
         ws.onopen = () => {
             resolve(ws)
@@ -59,85 +55,14 @@ const initTerminalWebSocket = async () => {
 const initXterm = () => {
     // 创建terminal实例
     term.open(document.getElementById('xterm-container'))
-    // 换行并输入起始符 $
-    const terminalPrompt = "~/Simblock$";
-    term.prompt = _ => {
-        term.write("\r\n\x1b[33m" + terminalPrompt + "\x1b[0m ")
-    }
-    term.prompt()
-    // canvas背景全屏
     const fitAddon = new FitAddon()
     term.loadAddon(fitAddon)
     fitAddon.fit()
-
-    window.addEventListener("resize", resizeScreen)
-    function resizeScreen() {
-        try { // 窗口大小改变时，触发xterm的resize方法使自适应
-            fitAddon.fit()
-        } catch (e) {
-            console.log("e", e.message)
-        }
-    }
-
-    runFakeTerminal(terminalPrompt.length)
+    const attachAddon = new AttachAddon(ws)
+    term.loadAddon(attachAddon)
 }
 
-const runFakeTerminal = (promptLength) => {
-    if (term._initialized) return
-    // 初始化
-    term._initialized = true
-    term.writeln("Welcome to \x1b[1;31mSimblock Terminal\x1b[0m.")
-    term.writeln("  ____    _   _     _   ____    _       ____    ____   _    _")
-    term.writeln(" / ___|  | | |  \\  / | | __ )  | |     / __ \\  / ___| | | / /")
-    term.writeln(" \\___ \\  | | | | \\/  | |  _ \\  | |    | |  | | | |    | |/ /")
-    term.writeln("  ___) | | | | |\\  / | | |_) | | |__  | |__| | | |__  | |\\ \\")
-    term.writeln(" |____/  |_| |_| \\/|_| |____/  |____|  \\____/  \\____| |_| \\_\\")
 
-    term.prompt()
-
-    let cmd = "";
-    // 添加事件监听器，支持输入方法
-    term.onKey(e => {
-        const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
-        if (e.domEvent.keyCode === 13) {       // 回车
-            runCommand(cmd);
-            cmd = "";
-            // term.prompt();
-        } else if (e.domEvent.keyCode === 8) { // 删除
-            if (term._core.buffer.x > promptLength + 1) {
-                term.write('\b \b');
-                cmd = cmd.slice(0, cmd.length - 1);
-                // console.log(cmd);
-            }
-        } else if (printable) {                // 输入一个字符
-            term.write(e.key);
-            // console.log(e.key);
-            cmd += e.key;
-        }
-        // console.log(1, 'print', e.key)
-    })
-    term.onData(key => {  // 粘贴的情况
-        if (key.length > 1) term.write(key);
-    })
-}
-
-const runCommand = (cmd) => {
-    if (cmd === "clear") {
-        term.clear()
-        term.prompt()
-        return
-    }
-    if (cmd.length === 0 || !ws) {
-        term.prompt();
-        return
-    }
-    ws.send(
-        JSON.stringify({
-            isClose: false,
-            cmd,
-        })
-    );
-}
 </script>
 
 <style></style>
