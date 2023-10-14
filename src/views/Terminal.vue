b61e078e2d9732aee2ecc25f74248d117cb8eba0
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2023-10-14 11:48:21 +0800

    fix: terminal size

--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <link rel="stylesheet" href="node_modules\xterm\css\xterm.css" />
+        <link rel="stylesheet" href="../../node_modules/xterm/css/xterm.css" />
         <div id="xterm-container"></div>
     </div>
 </template>
@@ -10,26 +10,10 @@ import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit'
 import { AttachAddon } from 'xterm-addon-attach'
 import { ref, onMounted, reactive } from 'vue';
-import axios from "axios";
-
-const terminalRows = 40;
-const terminalCols = 100;
 
 const term = new Terminal({
-    rendererType: "canvas", //渲染类型
-    rows: terminalRows, //行数
-    cols: terminalCols, // 不指定行数，自动回车后光标从下一行开始
-    convertEol: true, //启用时，光标将设置为下一行的开头
-    // scrollback: 50, //终端中的回滚量
-    disableStdin: false, //是否应禁用输入
-    // cursorStyle: "underline", //光标样式
-    cursorBlink: true, //光标闪烁
-    theme: {
-        foreground: "#ECECEC", //字体
-        background: "#000000", //背景色
-        cursor: "help", //设置光标
-        lineHeight: 20
-    }
+    rows: 40,
+    cols: 160,
 })
 
 let ws = null;
@@ -55,14 +39,9 @@ const initTerminalWebSocket = async () => {
 const initXterm = () => {
     // 创建terminal实例
     term.open(document.getElementById('xterm-container'))
-    const fitAddon = new FitAddon()
-    term.loadAddon(fitAddon)
-    fitAddon.fit()
     const attachAddon = new AttachAddon(ws)
     term.loadAddon(attachAddon)
 }
 
 
 </script>
-
-<style></style>
